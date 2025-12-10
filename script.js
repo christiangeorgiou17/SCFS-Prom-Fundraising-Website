43d0c00c969e89d04e3a06c8e6a43a0a10062347
christiangeorgiou17 <christiangeorgiou17@gmail.com>
2025-12-10 16:13:36 +0000

    Refactor fetch calls and add failsafe handling

--- script.js
+++ script.js
@@ -12,7 +12,33 @@ fetch('totalmoneyraised.json')
     console.error('There was a problem with the fetch operation:', error);
   });
 
+fetch('failsafe.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(data => {
+    toLock = data.lock;
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
 
 let oldHelpMessage = "Donations are welcome and thank you so much for your help. There also are other ways of helping like our bake sale with very delicious cakes and cookies!"
 
 //document.getElementById("money-raised-value").innerHTML = moneyRaisedValue;
+
+const Elements = [
+  document.getElementById("home"),
+  document.getElementById("money-raised-section"),
+  document.getElementById("how-can-you-help")
+]
+
+if (toLock) {
+  for (x = 0, x < 3, x ++) {
+    Elements[x].remove()
+  }
+  console.log("Lock")
+}
